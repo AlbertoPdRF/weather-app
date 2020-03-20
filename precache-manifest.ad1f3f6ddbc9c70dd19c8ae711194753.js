390123d0a9a82dba97000c2dade15adb14bfa0e0
Alberto Pérez de Rada Fiol <albertopdrf.wca@gmail.com>
2020-03-20 21:41:30 +0100

    Updates

---
+++ precache-manifest.ad1f3f6ddbc9c70dd19c8ae711194753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03c99eff1a88e3cfdaae8abe37c7f792",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e46b8ec5725c763f4386",
+    "url": "/weather-app/static/css/main.cfc2aed2.chunk.css"
+  },
+  {
+    "revision": "e907fbbef227742fc428",
+    "url": "/weather-app/static/js/2.14e25c9b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.14e25c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e46b8ec5725c763f4386",
+    "url": "/weather-app/static/js/main.ece0c24f.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  }
+]);
